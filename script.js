6222f8c2185d554c498af624843dac0035d29e94
Kelvin-Ben <kevoben14@gmail.com>
2023-05-13 20:11:18 +0300

    Create a function to take user inputs

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const addBook = document.querySelector('.add-book');
+const section2 = document.querySelector('.section__2')
 const showForm = document.querySelector('.container')
-
-addBook.addEventListener('click', function() {
-  showForm.classList.add('visible')
-  // showForm.classList.remove('visible');
-})
-
 const checkbox = document.querySelector('.mycheckbox')
 const checkBoxLabel = document.querySelector('#checkboxLabel');
+const form = document.querySelector('.form')
 
+const inputBook = document.getElementById('book');
+const inputName = document.getElementById('name');
+const inputPage = document.getElementById('pages');
+const inputConfirm = document.getElementById('confirm');
+
+
+
+
+document.addEventListener('click', function(event) {
+  if (!section2.contains(event.target) && event.target !== addBook) {
+    section2.style.display = 'none'
+  }
+})
+
+addBook.addEventListener('click', function() {
+  section2.style.display = 'flex'
+  console.log("clicked");
+})
 checkbox.addEventListener('change', function() {
   if (checkbox.checked) {
     checkBoxLabel.textContent = "Yes";
@@ -16,3 +30,27 @@ checkbox.addEventListener('change', function() {
     checkBoxLabel.textContent = "No"
   }
 });
+let myLibrary = [];
+function Book() {
+
+}
+function addBookToLibrary(event) {
+  event.preventDefault();
+
+  // create user object
+const book = {
+  title: inputBook.value,
+  name: inputName.value,
+  page: inputPage.value,
+  isRead: inputConfirm.value
+};
+
+// clear the form after submit
+inputBook.value = ''
+inputName.value = ''
+inputPage.value = ''
+// Add book object to the array
+myLibrary.push(book);
+console.log(myLibrary)
+}
+form.addEventListener('submit', addBookToLibrary)
